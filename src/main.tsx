6d0a52ae44fa6b488c2f034694e031a393154526
divyarang26 <divya.rang@imentus.com>
2025-08-06 15:39:51 +0530

    merchent only

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store'; // adjust the path if needed
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
--- src/smartWalletSlice.ts
+++ src/smartWalletSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = "https://2074e87f4752.ngrok-free.app";
+
 
 interface BuildPaymentRequest {
   wallet_address: string;
